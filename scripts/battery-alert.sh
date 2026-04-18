00279796a0c9d4c720dfb33ba9ae89085b4e279f
sendou <janan.karur@gmail.com>
2026-04-19 00:35:10 +0530

    battery success

--- scripts/battery-alert.sh
+++ scripts/battery-alert.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-THRESHOLD=79
+THRESHOLD=15
+
+NOTIFY="/run/current-system/sw/bin/notify-send"
+PAPLAY="/run/current-system/sw/bin/paplay"
 
 while true; do
     battery=$(cat /sys/class/power_supply/BAT1/capacity)
     status=$(cat /sys/class/power_supply/BAT1/status)
 
     if [ "$status" = "Discharging" ] && [ "$battery" -le "$THRESHOLD" ]; then
-        notify-send "Low Battery" "Battery is at ${battery}%"
-        paplay /run/current-system/sw/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+        $NOTIFY "Low Battery" "Battery is at ${battery}%"
+        $PAPLAY /run/current-system/sw/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
     fi
 
     sleep 60
